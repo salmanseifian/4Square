b2734c8282bcff0abc1480fbb6e900e45327cf46
Salman <salman.seifian@gmail.com>
2021-08-21 14:54:01 +0430

    Create Api Module

---
+++ app/src/main/java/com/salmanseifian/foursquare/FSApiModule.kt
@@ -0,0 +1,43 @@
+package com.salmanseifian.foursquare
+
+import com.salmanseifian.foursquare.data.remote.FSService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class FSApiModule {
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): FSService =
+        retrofit.create(FSService::class.java)
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .baseUrl(FSService.BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(): OkHttpClient {
+        val loggingInterceptor =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+}
