13bde87425cb1062156eeca872e2b04f7eedce4b
Salman <salman.seifian@gmail.com>
2021-08-24 12:55:50 +0430

    Write test for webserver with mock web server

--- app/src/test/java/com/salmanseifian/foursquare/data/remote/ApiServiceTest.kt
+++ app/src/test/java/com/salmanseifian/foursquare/data/remote/ApiServiceTest.kt
@@ -2,43 +2,72 @@ package com.salmanseifian.foursquare.data.remote
 
 import com.salmanseifian.foursquare.sampleLatLng
 import kotlinx.coroutines.runBlocking
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import org.junit.Assert
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okio.buffer
+import okio.source
+import org.amshove.kluent.internal.assertEquals
+import org.amshove.kluent.internal.assertNotEquals
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
-@RunWith(JUnit4::class)
 class ApiServiceTest {
+    private val mockWebServer = MockWebServer()
 
     private lateinit var apiService: ApiService
 
 
     @Before
     fun createService() {
-        val okHttpClient = OkHttpClient.Builder()
-        okHttpClient.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-
         apiService = Retrofit.Builder()
-            .baseUrl(ApiService.BASE_URL)
+            .baseUrl(mockWebServer.url("/"))
             .addConverterFactory(GsonConverterFactory.create())
-            .client(okHttpClient.build())
             .build()
             .create(ApiService::class.java)
     }
 
 
+    @After
+    fun stopService() {
+        mockWebServer.shutdown()
+    }
+
+
     @Test
-    fun searchVenues() {
-        val response = runBlocking {
-            apiService.searchVenues(ll = sampleLatLng)
+    fun `should return venues on success`() = runBlocking {
+
+        enqueueResponse("venues.json")
+
+        val response = apiService.searchVenues(ll = sampleLatLng)
+        
+        assertEquals(200, response.meta?.code)
+        assertNotEquals(null, response.response)
+        assertNotEquals(null, response.response?.venues)
+        
+        
+        
+    }
+
+
+    private fun enqueueResponse(fileName: String, headers: Map<String, String> = emptyMap()) {
+        val inputStream = javaClass.classLoader!!
+            .getResourceAsStream("api-response/$fileName")
+        val source = inputStream.source().buffer()
+
+        val mockResponse = MockResponse()
+
+        for ((key, value) in headers) {
+            mockResponse.addHeader(key, value)
         }
-        Assert.assertEquals(200, response.meta?.code)
+
+        mockWebServer.enqueue(
+            mockResponse
+                .setBody(source.readString(Charsets.UTF_8))
+        )
     }
 
 
